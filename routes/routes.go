4903e06f5a3995af0bfa6e1f83248a87bdd51219
adwin-nugroho <adwinnugroho16@gmail.com>
2021-11-23 13:07:55 +0800

    fix main navbar

--- routes/routes.go
+++ routes/routes.go
@@ -29,17 +29,26 @@ func Init(e *echo.Echo, services getRoutesDao) {
 	route := &GetRoutes{services}
 	// init index
 	indexRoute := e.Group("/")
-	indexRoute.GET("", route.index)
+	indexRoute.GET("", route.index2)
 	indexRoute.GET("about", route.about)
+	indexRoute.GET("blog", route.blog)
 	indexRoute.GET("contact", route.contact)
 	indexRoute.GET("faq", route.faq)
+	indexRoute.GET("event", route.event)
+	indexRoute.GET("privacy", route.privacy)
+	indexRoute.GET("product", route.product)
+
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func (route *GetRoutes) index(c echo.Context) error {
+func (route *GetRoutes) index2(c echo.Context) error {
+	var err error
+	if err != nil {
+		return c.JSON(404, "Error Not Found")
+	}
 	return c.Render(200, "home", map[string]interface{}{
 		"title": "e-Tetika | Wadah Etam Berkespresi",
 	})
@@ -51,14 +60,38 @@ func (route *GetRoutes) about(c echo.Context) error {
 	})
 }
 
+func (route *GetRoutes) blog(c echo.Context) error {
+	return c.Render(200, "blog", map[string]interface{}{
+		"title": "Blog | e-Tetika",
+	})
+}
+
 func (route *GetRoutes) contact(c echo.Context) error {
 	return c.Render(200, "contact", map[string]interface{}{
 		"title": "Contact | e-Tetika",
 	})
 }
 
+func (route *GetRoutes) event(c echo.Context) error {
+	return c.Render(200, "event", map[string]interface{}{
+		"title": "Event | e-Tetika",
+	})
+}
+
 func (route *GetRoutes) faq(c echo.Context) error {
 	return c.Render(200, "faq", map[string]interface{}{
 		"title": "Frequently Asked Question | e-Tetika",
+	})
+}
+
+func (route *GetRoutes) privacy(c echo.Context) error {
+	return c.Render(200, "privacy", map[string]interface{}{
+		"title": "Privacy | e-Tetika",
+	})
+}
+
+func (route *GetRoutes) product(c echo.Context) error {
+	return c.Render(200, "product", map[string]interface{}{
+		"title": "Product | e-Tetika",
 	})
 }
